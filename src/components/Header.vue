57b08fdef8cbd83fd58c5aa6c2aa57482f356c93
Ana <cauecvx@gmail.com>
2025-07-22 12:41:49 -0300

    Header sidebar e remoção dos arquivos padrão do vue

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+
+
+
+<template>
+
+<header>
+    <header class="header">
+        <h1 class="app-name"> <span class="app-name-color">Fin</span>App</h1>
+        <h1 class="title">
+           Painel <span class="title-color">Financeiro</span>
+        </h1>
+    </header>
+
+</header>
+
+</template>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,29 @@
+<script>
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+    <nav class="sidebar">
+
+        <div class="nav-buttons"> 
+            <RouterLink class="nav-button">
+                <i class="pi pi-microsoft icon"> </i>
+                <div class="nav-button-text">Overview</div>
+            </RouterLink>
+
+            <RouterLink class="nav-button-disabled">
+                <i class="pi pi-dollar icon"> </i>
+                <div class="nav-button-text">Transações</div>
+            </RouterLink>
+            <RouterLink class="nav-button-disabled">
+                <i class="pi pi-receipt icon"> </i>
+                Relatórios
+            </RouterLink>
+            <RouterLink class="nav-button-disabled">
+                <i class="pi pi-tags icon"> </i>
+                Categorias
+            </RouterLink>
+        </div>
+    </nav>
+</template>
