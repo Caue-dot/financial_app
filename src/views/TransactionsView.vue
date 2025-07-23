29bb6d7cacb5bbaa71da681d52fed78b4975a0ca
Ana <cauecvx@gmail.com>
2025-07-23 14:13:17 -0300

    netlify config

--- src/views/TransactionsView.vue
+++ src/views/TransactionsView.vue
@@ -30,10 +30,11 @@ import TransactionListing from '@/components/TransactionListing.vue';
       </span>
 
       <div class="transaction-side-boxes">
-        <span class="transactions-box-2">
+        <span class="transactions-box-3">
           <div class="layout">
-            <div class="transaction-add-title">Adicionar Transação</div>
-            <RouterLink class="transaction-add-link">Adicionar</RouterLink>
+              <div class="transaction-total-title">Resumo</div>
+              <div>Renda total: <span class="transaction-value-income">100</span></div>
+              <div>Gasto total: <span class="transaction-value-expense">100</span></div>
           </div>
         </span>
         <span class="transactions-box-2">
